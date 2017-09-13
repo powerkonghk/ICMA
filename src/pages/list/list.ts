d3dfc7733407f7eb769d3acf0150135fc0962fc0
Jimmy <jimmy@JimmytekiMacBook-Air.local>
2017-09-13 18:43:26 +0800

    select all contact

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { WebService } from '../service/web-service';
-import {SharePage} from '../share/share';
+import { SharePage } from '../share/share';
 
 @Component({
   selector: 'page-list',
@@ -11,14 +11,14 @@ import {SharePage} from '../share/share';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{ title: string, note: string, icon: string }>;
   contactList: Array<any>;
   sharePage = SharePage;
-  item : any;
-  selectedAll :boolean;
-  selectedContact: Array<any>;
+  item: any;
+  selectedAll: boolean;
+  selectedContact: Array<any> = new Array();
 
-  selectedName: Array<any>;
+  //selectedName: Array<any>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private webService: WebService) {
     this.getContactList();
@@ -36,11 +36,11 @@ export class ListPage {
 
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-    
+
     // Let's populate this page with some filler content for funzies
     // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     // 'american-football', 'boat', 'bluetooth', 'build'];
-    
+
     // this.items = [];
     // for (let i = 1; i < 11; i++) {
     //   this.items.push({
@@ -49,10 +49,10 @@ export class ListPage {
     //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
     //   });
     // }
-    
+
   }
 
-  checkAll(){
+  checkAll() {
     // if(this.contacts == true){
     //   this.contacts = false;
     // }else{
@@ -61,7 +61,7 @@ export class ListPage {
     // if(this.item.checked==false){
     //   this.item.checked==true;
     // }
-    
+
     //console.log(this.item.checked);
     // if (this.selectedAll) {
     //   this.selectedAll = true;
@@ -69,43 +69,48 @@ export class ListPage {
     //     this.selectedAll = false;
     // }
     //console.log(this.selectedAll);
-    for(let item of this.contactList) {
+    for (let item of this.contactList) {
       item.selected = this.selectedAll;
     }
   }
 
 
   getContactList() {
-		//console.log(event.target.value);
-		//if(event.target.value.length > 2) {
-			this.webService.getService().subscribe(
-				data => {
-					this.contactList = data; 
-					//console.log(data);
-				},
-				err => {
-					console.log(err);
-				},
-				() => console.log('Data fetched')
-			);
-		//}
-	}   
+    //console.log(event.target.value);
+    //if(event.target.value.length > 2) {
+    this.webService.getService().subscribe(
+      data => {
+        this.contactList = data;
+        //console.log(data);
+      },
+      err => {
+        console.log(err);
+      },
+      () => console.log('Data fetched')
+    );
+    //}
+  }
 
-  updateSelectedContact(){
+  updateSelectedContact() {
     // for(let item of this.contactList) {
     //   console.log(item.name);
     //   console.log(item.corpid);
     // }
-
-    console.log(this.selectedName);
-
+    this.selectedContact = new Array();
+    for (let item of this.contactList) {
+      if (item.selected == true) {
+        this.selectedContact.push(item.corpid);
+        console.log("::::" + item.corpid + "::::" + item.selected);
+      }
+    }
+    console.log("array!"+this.selectedContact);
   }
 
-  submitCase(){
+  submitCase() {
     //for(let item of this.contactList) {
-      //console.log("aaaa"+item.selected);
-      //this.selectedContact.push(this.item.selected.name);
-      // this.webService.selectedContact.push(this.item.selected.name);
+    //console.log("aaaa"+item.selected);
+    //this.selectedContact.push(this.item.selected.name);
+    // this.webService.selectedContact.push(this.item.selected.name);
     //} 
     //this.webService.pushSelectedContact(this.selectedContact);
     //this.webService.getSelectedContact();
