ef749545be40dee9d4e29d2e623e5687aad3bf86
Jimmy <jimmy@JimmytekiMacBook-Air.local>
2017-09-14 13:54:43 +0800

    finish with result page

---
+++ src/pages/result/result.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'result.html'
+})
+export class ResultPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
